5c6d4403a745e8816de6617220c482093ae56ab0
Davi Belo <davibelocontato@gmail.com>
2022-07-07 17:41:43 -0300

    first commit

---
+++ index.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main(){
+    float dist;
+    cin>>dist;
+    float jdist;
+    cin>>jdist;
+
+    float a = dist/jdist;
+    cout<<ceil(a)<<endl;
+        
+    return 0;
+}
